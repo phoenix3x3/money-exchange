623a196abcad4dbe220d0df42ca867bf0915445f
phoenix3x3 <fenix3x3@mail.ru>
2019-03-24 00:20:55 +0300

    feat: - add solution of task MoneyExchange in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    result = {};
+    const coins = [50, 25, 10, 5, 1];
+    const names = ['H', 'Q', 'D', 'N', 'P'];
+    let x;
+    const returnError = {
+        error: "You are rich, my friend! We don't have so much coins for exchange"
+    };
+    if(currency > 10000) return returnError;
+    if(currency < 0) return result;
+    for(let i = 0; i < 5; i++)  {
+        x = Math.floor(currency / coins[i]);
+        if(x != 0) result[names[i]] = x;
+        currency -= x * coins[i];
+    }
+    return result;
 }
